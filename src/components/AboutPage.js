6f3fe278eb1439912d205432cf99fc6a9d1a5fa1
Suprit Yentruvi <Suprityentruvi2003@gmail.com>
2024-11-12 12:00:04 +0530

    pushing repo

---
+++ src/components/AboutPage.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import styled, { keyframes, ThemeProvider } from 'styled-components'
+import {DarkTheme} from './Themes';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+import astronaut from '../assets/Images/spaceman.png'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+overflow: hidden;
+`
+const float = keyframes`
+0% { transform: translateY(-10px) }
+50% { transform: translateY(15px) translateX(15px) }
+100% { transform: translateY(-10px) }
+
+`
+const Spaceman = styled.div`
+position: absolute;
+top: 10%;
+right: 5%;
+width: 20vw;
+animation: ${float} 4s ease infinite;
+img{
+    width: 100%;
+    height: auto;
+}
+`
+const Main =  styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  padding: 2rem;
+  width: 50vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(0.6rem + 1vw);
+ backdrop-filter: blur(4px);
+  
+  position: absolute;
+  left: calc(5rem + 5vw);
+  top: 10rem;
+  font-family: 'Ubuntu Mono', monospace;
+  font-style: italic;
+`
+
+
+
+
+const AboutPage = () => {
+    return (
+        <ThemeProvider theme={DarkTheme}>
+<Box>
+
+<LogoComponent theme='dark'/>
+<SocialIcons theme='dark'/>
+<PowerButton />
+<ParticleComponent theme='dark' />
+
+        <Spaceman>
+            <img src={astronaut} alt="spaceman" />
+        </Spaceman>    
+        <Main>
+        As a machine learning developer based in India, I enjoy building intelligent solutions with a focus on data-driven insights and efficient performance.
+<br /> <br/>
+Passionate about the entire machine learning pipeline, I love experimenting with new algorithms, tools, and frameworks to create impactful projects.
+<br/> <br/>
+I’m also an independent freelancer and blogger, sharing insights on technology and the art of innovation. For me, every project is a craft, shaped with care and intent. Connect with me via social links to explore ideas and collaborations! 
+
+
+        </Main>
+
+        <BigTitle text="ABOUT" top="10%" left="5%" />
+        <a href="https://drive.google.com/file/d/1OljVWwpb7KUOu_ayR1KaCqo_h2TKQDFD/view?usp=sharing" target="_blank" class="resume-link">View Resume</a>
+
+
+
+        </Box>
+        
+
+        </ThemeProvider>
+        
+    )
+}
+
+export default AboutPage
---
+++ src/data/WorkData.js
@@ -0,0 +1,43 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Completed an internship in Python and machine learning! ",
+        description:"During this time, I learned how to create algorithms, analyze data, and build predictive models.",
+        tags:[],
+        
+        demo:"https://www.linkedin.com/posts/suprit-yentruvi-370787244_aspiring-data-scientist-with-practical-experience-activity-7208832137322524672-rD0s?utm_source=share&utm_medium=member_desktop"
+       
+    },
+    {
+        id:2,
+        name:" Completed my 2-month internship in Full Stack Development! ",
+        description:"This journey has been a deep dive into coding, problem-solving, and continuous learning. ",
+        tags:[],
+        
+        demo:"https://www.linkedin.com/posts/suprit-yentruvi-370787244_thrilled-to-have-completed-my-2-month-internship-activity-7230460491398008832-M44M?utm_source=share&utm_medium=member_desktop"
+        
+        
+    },
+    {
+        id:3,
+        name:"Flask Python",
+        description:"I have successfully completed the Flask Python course offered by Great Learning Academy.",
+        tags:[],
+        
+        demo:"https://www.linkedin.com/posts/suprit-yentruvi-370787244_flask-python-course-completion-certificate-activity-7247648172787818496-VovT?utm_source=share&utm_medium=member_desktop"
+        
+    },
+    {
+        id:4,
+        name:"SQL course on Udemy",
+        description:"Completed data querying and manipulation to more complex topics like joins, aggregations, and database management.",
+        tags:[],
+        
+        demo:"https://www.linkedin.com/posts/suprit-yentruvi-370787244_excited-to-share-that-ive-just-completed-activity-7261277550184718336-dyyT?utm_source=share&utm_medium=member_desktop"
+        
+       
+    },
+    
+] 
